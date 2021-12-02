68530bed96065dbd1c1d7d8a6a281cfc5b4055d9
Rafael Parana <rafael.parana@vizir.com.br>
2021-12-02 16:40:24 -0300

    feat: add patch route

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import faker from "faker";
 
 const app = express();
 
+app.use(express.json());
 app.get("/v1/payment-account/:id", (request: Request, response: Response) => {
   const array = [];
   for (let i = 0; i < faker.datatype.number({ min: 1, max: 4 }); i++) {
@@ -16,6 +17,18 @@ app.get("/v1/payment-account/:id", (request: Request, response: Response) => {
     });
   }
   return response.send(array);
+});
+
+app.patch("/v1/payment-account/:id", (request: Request, response: Response) => {
+  console.log(request.body);
+  return response.send({
+    accountDetails: {
+      clabe: request.body.clabe,
+    },
+    countryCode: faker.address.countryCode(),
+    companyId: faker.datatype.number({ min: 1, max: 2 }),
+    personId: request.params.id,
+  });
 });
 
 app.listen(process.env.PORT || 3333);
